190c514b951878a79756a51c68aaaa482c33b333
and0ne808 <and0ne808@gmail.com>
2017-02-05 15:57:51 -0700

    All materials
    Upload all unity stuff

---
+++ Hero2/Assets/Scripts/SoundOnClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundOnClick : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        // audio.PlayOneShot( yourAudioclip)
+
+
+    }
+
+    void OnMouseDown()
+    {
+        audioSource.Play();
+
+    }
+}
